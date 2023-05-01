ad01915e7068b6602be353dab1e914569b9e4a13
osmar <osmarernesto17@gmail.com>
2023-05-01 00:44:59 -0600

    producto

---
+++ ejericicio7.py
@@ -0,0 +1,18 @@
+def calcular_producto(a, b):
+    if a > b or a == 0 or b == 0:
+        return 0
+    else:
+        producto = 1
+        for i in range(a, b+1):
+            producto *= i
+        return producto
+
+a = int(input("Ingresa el valor de a: "))
+b = int(input("Ingresa el valor de b: "))
+
+resultado = calcular_producto(a, b)
+
+if resultado == 0:
+    print("El producto no se puede calcular porque a es mayor que b o porque a o b es igual a cero.")
+else:
+    print(f"El producto de los números enteros desde {a} hasta {b} es {resultado}.")
